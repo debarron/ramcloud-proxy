7b194045e2dc668442bd20d10dcce66a12916ff5
Daniel Lopez <de.lopezbarron@gmail.com>
2020-04-04 21:04:13 -0500

    Automatic commit by debarron

--- src/RCProxyTest.cc
+++ src/RCProxyTest.cc
@@ -8,12 +8,128 @@ using namespace std;
 
 #define CLUSTER_NAME "_rctest_"
 
+void testSinglePushSinglePull(RCProxy &client){
+  vector<RCEntry> number;
+  vector<string> numKey;
+  RCTable *dataTest;
+  RCRelation *dataOut;
+  uint32_t valueIn, valueOut;
+  double timePush, timePull;
+ 
+  valueIn = 69;
+  dataTest = client.createTable("numTesting", 5);
+  number.push_back(RCEntry("test", (char *)&value, sizeof(uint32_t)));
+
+  RCRelation dataInput (dataTest, &number);
+  client.push(dataInput);
+  timePush = client.elapsedSecsLog;
+
+  numKey.push_back("test");
+  dataOut = client.pull(dataTest, numKey);
+  timePull = client.elapsedSecsLog;
+
+  valueOut = *((uint32_t *)(*dataOut->entries)[0].data);
+
+  cout << ">> ramcloud-proxy SINGLE PUSH - SINGLE PULL EXPERIMENT\N";
+    << "TABLE, VALUE_IN, VALUE_OUT, EQUAL, TIME_PUSH, TIME_PULL\n"
+    << "numTesting, " 
+    << valueIn << ", "
+    << valueOut << ", "
+    << (valueIn == valueOut) << ", "
+    << timePush << ", "
+    << timePull <<"\n";
+}
+
+
+void testingMultiPushSinglePull(RCProxy &client){
+  vector<RCEntry> numbers;
+  vector<string> numKeys;
+  vector<string> pullKey;
+  RCTable *dataTest;
+  RCRelation *dataOut;
+  double timePush, timePull;
+  uint32_t valuesIn[5] = {1, 2, 3, 4, 5};
+  uint32_t valueOut;
+ 
+  dataTest = client.createTable("numbersTesting", 5);
+  for(uint32_t i = 0; i < 5; i++){
+    string key = "key" + to_string(i);
+    numbers.push_back(RCEntry(key, (char *)&valuesIn[i], sizeof(uint32_t)));
+    keys.push_back(key);
+  }
+  RCRelation dataInput (dataTest, &numbers);
+  client.push(dataInput);
+  timePush = client.elapsedSecsLog;
+
+  cout << ">> ramcloud-proxy MULTI PUSH - SINGLE PULL EXPERIMENT\n";
+  cout << "TABLE, #VALUES_IN, TIME_PUSH\n";
+  cout << "numbersTesting, 5, " << timePush <<"\n\n";
+
+  cout << "TABLE, VALUE_OUT, VALUE_EXPECTED, IS_EQUAL, TIME_PULL\n";
+  for(uint32_t i = 0; i < 5; i++){
+    pullKey.clear();
+    pullKey.push_back(numKeys[i]);
+
+    dataOut = client.pull(dataTest, pullKey);
+    valueOut = *((uint32_t *)(*dataOut->entries)[0].data);
+    timePull = client.elapsedSecsLog;
+
+    cout << "numbersTesting, " 
+      << valueOut << ", "
+      << valuesIn[i] << ", "
+      << (valueOut == valuesIn[i]) << ", "
+      << timePull << "\n";
+  }
+}
+
+
+void testingMultiPushMultiPull(RCProxy &client){
+  vector<RCEntry> numbers;
+  vector<string> numKeys;
+  RCTable *dataTest;
+  RCRelation *dataOut;
+  double timePush, timePull;
+  uint32_t valuesIn[5] = {1, 2, 3, 4, 5};
+  uint32_t valuesOut[5];
+ 
+  dataTest = client.createTable("numbersTestingM", 5);
+  for(uint32_t i = 0; i < 5; i++){
+    string key = "key" + to_string(i);
+    numbers.push_back(RCEntry(key, (char *)&valuesIn[i], sizeof(uint32_t)));
+    keys.push_back(key);
+  }
+  RCRelation dataInput (dataTest, &numbers);
+  client.push(dataInput);
+  timePush = client.elapsedSecsLog;
+
+  cout << ">> ramcloud-proxy MULTI PUSH - MULTI PULL EXPERIMENT\n"
+    << "TABLE, #VALUES_IN, TIME_PUSH\n"
+    << "numbersTestingM, 5, " << timePush <<"\n\n";
+
+
+  dataOut = client.pull(dataTest, numKeys);
+  timePull = client.elapsedSecsLog;
+  for(uint32_t i = 0; i < 5; i++)
+    valuesOut[i] = *((uint32_t *)(*dataOut->entries)[i].data);
+
+  cout << "TABLE, #VALUES_FETCHED, TIME_PULL\n"
+    << "numbersTestingM, " 
+    << dataOut->entries->size() << ", "
+    << timePull << "\n\n";
+  
+  cout << "VALUE_EXPECTED, VALUE_FETCHED, EQUAL\n";
+  for(uint32_t i = 0; i < 5; i++)
+    cout << valuesIn[i] << ", " << valuesOut[i] << ", " << (valuesIn[i] == valuesOut[i]) << "\n";
+  
+
+}
+
+
+
+
 int main(int argc, char **argv){
   char *serviceLocator;
   double eTime;
-  RCTable *insertTest;
-  vector<RCEntry> entries;
-  vector<RCEntry> multiEntries;
 
   serviceLocator = argv[1];
 
@@ -26,83 +142,11 @@ int main(int argc, char **argv){
   eTime += rc.elapsedSecsLog;
   rc.createTable("Table3", 33);
   eTime += rc.elapsedSecsLog;
-  cout << "Avg time to create table: " << (eTime/3.0)  << " secs.\n";
- 
-  eTime = 0.0;
-  insertTest = rc.createTable("insertTest", 33);
-  eTime = rc.elapsedSecsLog;
-  cout << "Create table 'inserTest' with id " << insertTest->tableId << " took " << eTime << " secs.\n";
+  cout << "Avg time to create table: " << (eTime/3.0)  << " secs.\n\n";
 
-  eTime = 0.0;
-  string key = "myKey";
-  string value = "hello world";
-  char *vp = new char[value.size()+1];
-  strcpy(vp, value.c_str());
-
-  RCEntry e(key, vp, value.length());
-  entries.push_back(e);
-  RCRelation simpleInput(insertTest, &entries);
-  rc.push(&simpleInput);
-  eTime = rc.elapsedSecsLog;
-  cout << "Insert in 'inserTest' took " << eTime <<" secs\n";
-
-
-  int valuesSize = 4;
-  int values[4] = {1, 2, 3, 4};
-  multiEntries.push_back(RCEntry(string("key1"), (char *)&values[0], sizeof(int)));
-  multiEntries.push_back(RCEntry(string("key2"), (char *)&values[1], sizeof(int)));
-  multiEntries.push_back(RCEntry(string("key3"), (char *)&values[2], sizeof(int)));
-  multiEntries.push_back(RCEntry(string("key4"), (char *)&values[3], sizeof(int)));
-  RCRelation multiInput(insertTest, &multiEntries);
-  rc.push(&multiInput);
-  eTime = rc.elapsedSecsLog;
-  cout << "Insert in 'insertTest' " << valuesSize << " keys took " << eTime << " secs\n";
-
-  vector<string> singleKey;
-  singleKey.push_back(string("myKey"));
-  RCRelation *singleKeyData = rc.pull(insertTest, singleKey);
-  eTime = rc.elapsedSecsLog;
-  int totalKeys = singleKeyData->entries->size();
-  cout << "Pulling from 'insertTest' retrieved " << totalKeys << " keys took " << eTime << " secs\n";
-
-  vector<string> singleKey2;
-  singleKey2.push_back(string("key1"));
-  singleKeyData = rc.pull(insertTest, singleKey2);
-  eTime = rc.elapsedSecsLog;
-  totalKeys = singleKeyData->entries->size();
-  cout << "Pulling from 'insertTest' retrieved " << totalKeys << " keys took " << eTime << " secs\n";
-
-  vector<string> singleKey3;
-  singleKey3.push_back(string("key2"));
-  singleKeyData = rc.pull(insertTest, singleKey3);
-  eTime = rc.elapsedSecsLog;
-  totalKeys = singleKeyData->entries->size();
-  cout << "Pulling from 'insertTest' retrieved " << totalKeys << " keys took " << eTime << " secs\n";
-
-
-  vector<string> multiKey;
-  multiKey.push_back(string("key1"));
-  multiKey.push_back(string("key2"));
-  multiKey.push_back(string("key3"));
-  multiKey.push_back(string("key4"));
-  RCRelation *multiKeyData = rc.pull(insertTest, multiKey);
-  eTime = rc.elapsedSecsLog;
-  int totalMultiKey = multiKeyData->entries->size();
-  cout << "Pulling (multiKey) from 'insertTest' retrieved " << totalMultiKey << " keys took " << eTime << " secs\n";
-
-  // TODO
-  /*
-  vector<string> allKeys = rc.enumerateKeysFromTable(insertTest);
-  cout << "Listing all keys from 'insertTest'\n";
-  for(uint32_t i = 0; i < allKeys.size(); i++)
-    cout << "has key: " << allKeys[i] << "\n";
-  */
-
-  cout << "Dropping the tables\n";
-  rc.dropTable("insertTest");
-  rc.dropTable("Table1");
-  rc.dropTable("Table2");
-  rc.dropTable("Table3");
+  testSinglePushSinglePull(&client);
+  testingMultiPushSinglePull(&client);
+  testingMultiPushMultiPull(&client);
 
   return 0;
 }
