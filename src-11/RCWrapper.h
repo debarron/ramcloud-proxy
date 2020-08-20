4182be0b645de0d504bd6ac78e12ce97ab1dc744
Daniel Lopez <de.lopezbarron@gmail.com>
2020-08-19 20:13:26 -0600

    Automatic commit

--- src-11/RCWrapper.h
+++ src-11/RCWrapper.h
@@ -24,27 +24,28 @@ namespace RCWrapper{
   typedef map<uint64_t, vector<Entry>> Relation;
   typedef map<uint64_t, vector<Entry>>::iterator RelationIterator;
 
-  class RCWrapper{
-    private:
-      RamCloud *_client = NULL;
-      CommandLineOptions _options;
-      clock_t _start;
-      clock_t _end;
-      void _time_start();
-      void _time_end();
-      int _count_entries(Relation&);
-
-    public:
-      RCWrapper(string, string);
-      int close();
-      double elapsed_time();
-      uint64_t create_table(string, int);
-      uint64_t get_table_id(string);
-      /*int write(Relation&, int);
-      int write(uint64_t, string, char*, uint32_t);
-      Relation *read(Relation&);
-      Relation *read(uint64_t, string);*/
-  };
+  class RCWrapper;
 }
 
+class RCWrapper::RCWrapper{
+  private:
+    RamCloud *_client = NULL;
+    CommandLineOptions _options;
+    clock_t _start;
+    clock_t _end;
+    void _time_start();
+    void _time_end();
+    int _count_entries(RCWrapper::Relation&);
+
+  public:
+    RCWrapper(string, string);
+    int close();
+    double elapsed_time();
+    uint64_t create_table(string, int);
+    uint64_t get_table_id(string);
+    /*int write(Relation&, int);
+    int write(uint64_t, string, char*, uint32_t);
+    Relation *read(Relation&);
+    Relation *read(uint64_t, string);*/
+};
 #endif
