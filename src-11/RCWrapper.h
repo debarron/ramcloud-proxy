14d92876f1169bbbb532bd53468395f6f44f3e7f
Daniel Lopez <de.lopezbarron@gmail.com>
2020-08-19 23:11:56 -0600

    Automatic commit

--- src-11/RCWrapper.h
+++ src-11/RCWrapper.h
@@ -32,6 +32,7 @@ class RCWrapper{
     void _time_start();
     void _time_end();
     int _count_entries(Relation&);
+    void _multi_read_request(uint64_t, vector<Entry>&, MultiReadObject*, Tub<Buffer>*, MultiReadObject**);
 
   public:
     RCWrapper(string, string);
