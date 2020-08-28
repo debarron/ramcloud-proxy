7c22f542c3ebba0af8cfb4d6212e29c19f2e619c
Daniel Lopez <de.lopezbarron@gmail.com>
2020-08-28 16:53:46 -0600

    Automatic commit

--- src-11/RCWrapper_C.cpp
+++ src-11/RCWrapper_C.cpp
@@ -87,7 +87,7 @@ int RCWrapper_C_multi_write(RCRecord *records, int records_length, int steps){
 
   Relation *r = RCW_record_to_relation(records, records_length);
   wrapper->write(*r, steps);
-  success_writes = wrapper->count_entries(r);
+  success_writes = wrapper->count_entries(*r);
 
   RCW_free_relation(r);
   return success_writes;
