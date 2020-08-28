ab772de947444c98aa1fec510c6e3b1a328abf62
Daniel Lopez <de.lopezbarron@gmail.com>
2020-08-28 16:48:36 -0600

    Automatic commit

--- src-11/RCWrapper_C.cpp
+++ src-11/RCWrapper_C.cpp
@@ -4,47 +4,6 @@
 using namespace std;
 RCWrapper *wrapper = NULL;
 
-void RCWrapper_C_init(const char *service_locator, const char *cluster_name){
-  string _service_locator = string(service_locator);
-  string _cluster_name = string(cluster_name);
-
-  if(wrapper == NULL)
-    wrapper = new RCWrapper(_service_locator, _cluster_name);
-}
-
-uint64_t RCWrapper_C_create_table(const char *table_name, int server_span){
-  string _table_name = string(table_name);
-  return wrapper->create_table(_table_name, server_span);
-}
-
-uint64_t RCWrapper_C_get_table_id(const char *table_name){
-  string _table_name = string(table_name);
-  return wrapper->get_table_id(_table_name);
-}
-
-void RCWrapper_C_single_write(uint64_t table_id, const char *key, const char *value, uint32_t value_length){
-  string _key = string(key);
-  wrapper->write(table_id, _key, value, value_length);
-}
-
-int RCWrapper_C_multi_write(RCRecord *records, int records_length, int steps){
-  int success_writes = 0;
-
-  Relation *r = RCW_record_to_relation(records, records_length);
-  wrapper->write(r, steps);
-  success_writes = wrapper->count_entries(r);
-
-  RCW_free_relation(r);
-  return success_writes;
-}
-
-void RCW_free_relation(Relation *r){
-  for(RelationIterator it = r->begin(); it != r->end(); ++it)
-    it->second->clear();
-  r->clear();
-  delete r;
-}
-
 Relation *RCW_rcrecord_to_relation(RCRecord *records, int records_length){
   Relation *relation = new Relation();
   vector<Entry> *entries = new vector<Entry>();
@@ -92,6 +51,47 @@ RCRecord *RCW_relation_to_record(Relation *r){
   return result;
 }
 
+
+void RCWrapper_C_init(const char *service_locator, const char *cluster_name){
+  string _service_locator = string(service_locator);
+  string _cluster_name = string(cluster_name);
+
+  if(wrapper == NULL)
+    wrapper = new RCWrapper(_service_locator, _cluster_name);
+}
+
+uint64_t RCWrapper_C_create_table(const char *table_name, int server_span){
+  string _table_name = string(table_name);
+  return wrapper->create_table(_table_name, server_span);
+}
+
+uint64_t RCWrapper_C_get_table_id(const char *table_name){
+  string _table_name = string(table_name);
+  return wrapper->get_table_id(_table_name);
+}
+
+void RCWrapper_C_single_write(uint64_t table_id, const char *key, const char *value, uint32_t value_length){
+  string _key = string(key);
+  wrapper->write(table_id, _key, value, value_length);
+}
+
+int RCWrapper_C_multi_write(RCRecord *records, int records_length, int steps){
+  int success_writes = 0;
+
+  Relation *r = RCW_record_to_relation(records, records_length);
+  wrapper->write(*r, steps);
+  success_writes = wrapper->count_entries(r);
+
+  RCW_free_relation(r);
+  return success_writes;
+}
+
+void RCW_free_relation(Relation *r){
+  for(RelationIterator it = r->begin(); it != r->end(); ++it)
+    it->second->clear();
+  r->clear();
+  delete r;
+}
 char *RCWrapper_C_single_read(uint64_t table_id, const char *key, uint32_t *value_length){
   char *_value;
   string _key;
@@ -105,7 +105,7 @@ RCRecord *RCWrapper_C_multi_read(RCRecord *records, int records_length, int step
   int success_reads = 0;
 
   Relation *r = RCW_rcrecord_to_relation(records, records_length);
-  Relation *data_read = wrapper->read(r, steps, &success_reads);
+  Relation *data_read = wrapper->read(*r, steps, &success_reads);
   return RCW_relation_to_record(data_read); 
 }
 
